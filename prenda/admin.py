097e73c031080a805a5c91b343ac006ae5c18a01
Daniel Piñeiro <dpinheiro88@gmail.com>
2019-08-13 18:34:35 +0200

    Model Prenda
    Modelo de Prenda creado con atributos básicos para pruebas.
    Registrado en el admin

--- prenda/admin.py
+++ prenda/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Prenda
 
-# Register your models here.
+
+admin.site.register(Prenda)
--- prenda/models.py
+++ prenda/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+CAMISA = 'FR'
+CAMISETA = 'SO'
+PANTALON = 'JR'
+CALZADO = 'SR'
+
+TIPO_PRENDA = [
+    (CAMISA, 'camisa'),
+    (CAMISETA, 'camiseta'),
+    (PANTALON, 'pantalon'),
+    (CALZADO, 'calzado')
+]
+
+
+class Prenda(models.Model):
+    marca = models.CharField(
+        max_length=200
+    )
+
+    modelo = models.CharField(
+        max_length=200
+    )
+
+    tipo = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        choices=TIPO_PRENDA,
+        default=None
+    )
