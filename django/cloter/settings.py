45a0b62c57e3bc2d0905d8be6c80db7658317030
Daniel Piñeiro <dpinheiro88@gmail.com>
2020-04-10 17:28:03 +0200

    CORS configurado para localhost

--- django/cloter/settings.py
+++ django/cloter/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_extensions',
+    'corsheaders',
 
     'prenda',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+    'http://localhost:8080',
+)
